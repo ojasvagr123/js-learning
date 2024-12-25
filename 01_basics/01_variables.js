929e49648dc93066ac05fb8e09f8bcf4e06ceb25
ojasvagr123 <ojasvagrawal2807@gmail.com>
2024-12-25 06:15:17 +0000

    varibles in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=12223
+let accountName="ojasv Agrawal"
+var accountPass="1221"
+accountCity="Ratlam"
+
+//const can't be changed
+//can be declared without any keyword
+//var is not used because of the block scope
+//if not initialized than it shows undefined
+
+console.log(accountId);
+console.table([accountId,accountName,accountPass,accountCity])
