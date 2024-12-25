533fc43f7ef79dfd35065b6fed0eaf8ac7cd415f
ojasvagr123 <ojasvagrawal2807@gmail.com>
2024-12-25 10:31:06 +0000

    data Type conversion in js

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let score="33"
+
+console.log(typeof score);
+
+let value=Number(score)
+console.log(typeof value);
+console.log(value);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//false,null=>0
+
+let LoggedIN=""
+let valueOfLoggedIN=Boolean(LoggedIN);
+console.log(valueOfLoggedIN);
+
+//0=>false,1=>true
+//""=>false,"ojasv"=>true
+
+
+
+
+
+
