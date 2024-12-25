b784111fae293e9bf5a43b763f1cae3cd49610cc
ojasvagr123 <ojasvagrawal2807@gmail.com>
2024-12-25 06:28:08 +0000

    data tpes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+let age=18
+let name="ojasv"
+let city;
+
+//number
+//bigInt
+//string
+//undefined
+//null
+//boolean
+//symbol=>uniques-used mostly in react
+
+console.log(typeof null) //object
+console.log(typeof undefined);//undefined
