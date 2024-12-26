0f285a7306875c3879c5fd3c0785bff98e4c6279
ojasvagr123 <ojasvagrawal2807@gmail.com>
2024-12-26 17:02:40 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -18,6 +18,18 @@ console.log(valueOfLoggedIN);
 //0=>false,1=>true
 //""=>false,"ojasv"=>true
 
+let str2="hello"
+let str1=" ojasv"
+console.log(str1+str2);
+
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+
+
+
 
 
 
