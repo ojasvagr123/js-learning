c613fdff330fc347cacd6c6681ca8cce0c9b7404
ojasvagr123 <ojasvagrawal2807@gmail.com>
2025-03-25 11:02:14 +0000

    stings and its functions

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,21 @@
+const name = "ojasv"
+
+const repoCount=10
+
+//console.log(name+ repoCount);
+
+console.log(`hello my name is ${name}`);
+
+const gameName = new String('ojasv-game-new-com')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+const newString=gameName.slice(0,4)
+console.log(newString);
+console.log(gameName.replace('-','&'));
+const words=gameName.split('-')
+console.log(words);
+
+
+
+
+
