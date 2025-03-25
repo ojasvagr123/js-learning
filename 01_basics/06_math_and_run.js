506b8b797d0cc4e88f61036e5a07467527a50c70
ojasvagr123 <ojasvagrawal2807@gmail.com>
2025-03-25 12:02:26 +0000

    math in javascript

---
+++ 01_basics/06_math_and_run.js
@@ -0,0 +1,31 @@
+const score=1000
+console.log(score);
+
+const Balance=new Number(26123.88999);
+console.log(Balance);
+console.log(Balance.toFixed());
+console.log(Balance.toPrecision(3));
+console.log(Balance.toLocaleString('en-IN'));
+
+
+//+++++++Maths++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
